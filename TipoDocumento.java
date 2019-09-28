75dec7baed3a94710bf43057f6b029fc4d291546
vanderfonte <55927598+vanderfonte@users.noreply.github.com>
2019-09-28 16:10:40 -0300

    Create TipoDocumento.java

---
+++ TipoDocumento.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author vand
+ */
+public enum TipoDocumento {
+    
+    CPF {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cpf();
+        }
+    }, CNPJ {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cnpj();
+        }
+    };
+    
+    public abstract String geraNumeroTeste();
+}
